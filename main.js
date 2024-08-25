d858140d156d475e3b324d4cb3ad24cbb2b19e9c
DELLIORRA <116285814+DELLIORRA@users.noreply.github.com>
2024-08-25 12:35:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
-var myButton = document.querySelector("button");
-var myHeading = document.querySelector("p");
-function setUserName() {
-    var myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "Mozilla is cool, " + myName;
-  }
+
   
